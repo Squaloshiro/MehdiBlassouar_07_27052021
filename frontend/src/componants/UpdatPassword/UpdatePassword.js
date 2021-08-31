43e1f0147fd8489c2e9d8fe738ae09f904dcb10c
Squaloshiro <mehdi.link@hotmail.fr>
2021-08-31 18:11:18 +0200

    implémantation verif mdp usernam email + css page profil

--- frontend/src/componants/UpdatPassword/UpdatePassword.js
+++ frontend/src/componants/UpdatPassword/UpdatePassword.js
@@ -1,25 +1,159 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import api from "../../config/api";
 import Input from "../Input/Input";
 import Button from "../Button/Button";
 import { toastTrigger } from "../../helper/toast";
+import "./updatepassword.scss";
 const UpdadePassword = () => {
   const [oldPassword, setOldPasseword] = useState("");
   const [newPassword, setNewPassword] = useState("");
   const [valueError, setValueError] = useState("");
+  const [compteurOldPassword, setCompteurOldPassword] = useState(0);
+  const [classNameOldPassWord, setClassNameOldPassWord] = useState("");
+  const [compteurNewPassword, setCompteurNewPassword] = useState(0);
+  const [classNameNewPassWord, setClassNameNewPassWord] = useState("");
+  const [compteurUppercase, setCompteurUppercase] = useState(0);
+  const [classNameUppercase, setClassNameUppercase] = useState("");
+  const [compteurLowercase, setCompteurLowercase] = useState(0);
+  const [classNameLowercase, setClassNameLowercase] = useState("");
+  const [compteurNumber, setCompteurNumber] = useState(0);
+  const [classNameNumber, setClassNameNumber] = useState("");
+  const [compteurCharacter, setCompteurCharacter] = useState(0);
+  const [classNameCharacter, setClassNameCharacter] = useState("");
+  const [activeUppercase, setActiveUppercase] = useState(false);
+  const [active, setActive] = useState(false);
+  const clickOutSide = useRef();
+  const clickOutSideError = useRef();
+
+  const handleClickOutside = (e) => {
+    if (!clickOutSide.current?.contains(e.target)) {
+      setActiveUppercase(false);
+    }
+  };
+  const handleClickOutsideError = (e) => {
+    if (!clickOutSideError.current?.contains(e.target)) {
+      setActive(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  });
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutsideError);
+    return () => document.removeEventListener("mousedown", handleClickOutsideError);
+  });
+
+  useEffect(() => {
+    if (compteurOldPassword < 8) {
+      setClassNameOldPassWord("color-red");
+    } else {
+      setClassNameOldPassWord("color-green");
+    }
+    if (compteurNewPassword < 8) {
+      setClassNameNewPassWord("color-red");
+    } else {
+      setClassNameNewPassWord("color-green");
+    }
+    if (compteurUppercase < 1) {
+      setClassNameUppercase("color-red");
+    } else {
+      setClassNameUppercase("color-green");
+    }
+    if (compteurLowercase < 1) {
+      setClassNameLowercase("color-red");
+    } else {
+      setClassNameLowercase("color-green");
+    }
+    if (compteurNumber < 1) {
+      setClassNameNumber("color-red");
+    } else {
+      setClassNameNumber("color-green");
+    }
+    if (compteurCharacter < 1) {
+      setClassNameCharacter("color-red");
+    } else {
+      setClassNameCharacter("color-green");
+    }
+  }, [
+    compteurOldPassword,
+    compteurNewPassword,
+    compteurUppercase,
+    compteurLowercase,
+    compteurNumber,
+    compteurCharacter,
+  ]);
 
   const onChangeOldPassword = (e) => {
+    setCompteurOldPassword(e.target.value.length);
     setOldPasseword(e.target.value);
     setValueError("");
   };
 
   const onChangeNewPassWord = (e) => {
+    const regexUppercase = /[A-Z]/g;
+    let findLengthUppercase = e.target.value.match(regexUppercase);
+    if (findLengthUppercase) {
+      findLengthUppercase = findLengthUppercase.length;
+    }
+    const regexLowercase = /[a-z]/g;
+    let findLengthLowercase = e.target.value.match(regexLowercase);
+    if (findLengthLowercase) {
+      findLengthLowercase = findLengthLowercase.length;
+    }
+    const regexNumber = /[0-9]/g;
+    let findLengthNumber = e.target.value.match(regexNumber);
+    if (findLengthNumber) {
+      findLengthNumber = findLengthNumber.length;
+    }
+    const regexCharacter = /[_\W]/g;
+    let findLengthCharacter = e.target.value.match(regexCharacter);
+    if (findLengthCharacter) {
+      findLengthCharacter = findLengthCharacter.length;
+    }
+    setCompteurCharacter(findLengthCharacter);
+    setCompteurNumber(findLengthNumber);
+    setCompteurLowercase(findLengthLowercase);
+    setCompteurUppercase(findLengthUppercase);
+    setCompteurNewPassword(e.target.value.length);
     setNewPassword(e.target.value);
+    setActiveUppercase(true);
     setValueError("");
   };
 
   const updateProfilBio = async () => {
     const obj = { oldPassword, newPassword };
+    if (compteurOldPassword < 8) {
+      setActiveUppercase(false);
+      setActive(true);
+      return;
+    }
+    if (compteurNewPassword < 8) {
+      setActiveUppercase(false);
+      setActive(true);
+      return;
+    }
+    if (compteurUppercase < 1) {
+      setActiveUppercase(false);
+      setActive(true);
+      return;
+    }
+    if (compteurLowercase < 1) {
+      setActiveUppercase(false);
+      setActive(true);
+      return;
+    }
+    if (compteurNumber < 1) {
+      setActiveUppercase(false);
+      setActive(true);
+      return;
+    }
+    if (compteurCharacter < 1) {
+      setActiveUppercase(false);
+      setActive(true);
+      return;
+    }
     try {
       const token = JSON.parse(JSON.stringify(sessionStorage.getItem("groupomaniaToken")));
       await api({
@@ -28,8 +162,16 @@ const UpdadePassword = () => {
         data: obj,
         headers: { Authorization: `Bearer ${token}`, Accept: "application/json" },
       });
+      setCompteurOldPassword(0);
       setOldPasseword("");
       setNewPassword("");
+      setCompteurUppercase(0);
+      setCompteurNumber(0);
+      setCompteurLowercase(0);
+      setCompteurCharacter(0);
+      setActiveUppercase(false);
+      setCompteurNewPassword(0);
+      setActive(false);
       toastTrigger("success", "Mots de passe modifié");
     } catch (error) {
       toastTrigger("error", "une erreur est survenu");
@@ -38,14 +180,33 @@ const UpdadePassword = () => {
   };
 
   return (
-    <div>
-      <div>
+    <div ref={clickOutSide} className="size-update-password">
+      <div className="old-password-position">
         <Input onChange={onChangeOldPassword} type="password" value={oldPassword} label="Ancien mots de passe" />
+        <div>
+          {compteurOldPassword > 0 && (
+            <div className={classNameOldPassWord}>Minimum de caratère : {compteurOldPassword}/8</div>
+          )}
+        </div>
       </div>
       <div>
         <Input onChange={onChangeNewPassWord} type="password" value={newPassword} label="Nouveau mots de passe" />
+        <div>
+          {compteurNewPassword > 0 && (
+            <div className={classNameNewPassWord}>Minimum de caratère : {compteurNewPassword}/8</div>
+          )}
+        </div>
+        <div>{activeUppercase && <div className={classNameUppercase}>Minimum 1 majuscule </div>}</div>
+        <div>{activeUppercase && <div className={classNameLowercase}>Minimum 1 minuscule </div>}</div>
+        <div>{activeUppercase && <div className={classNameNumber}>Minimum 1 chiffre </div>}</div>
+        <div>{activeUppercase && <div className={classNameCharacter}>Minimum 1 charatère spécial </div>}</div>
       </div>
-      {valueError && <div>{valueError}</div>}
+      {active && (
+        <div ref={clickOutSideError} className="color-red">
+          Vériffiez vos information de saisie
+        </div>
+      )}
+      {valueError && <div ref={clickOutSideError}>{valueError}</div>}
       <div>
         <Button onClick={updateProfilBio} title="Envoyer" />
       </div>
--- frontend/src/pages/SignUp/SignUp.js
+++ frontend/src/pages/SignUp/SignUp.js
@@ -1,6 +1,6 @@
 import Input from "../../componants/Input/Input";
 import Button from "../../componants/Button/Button";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import api from "../../config/api";
 import { useHistory } from "react-router";
 import { toastTrigger } from "../../helper/toast";
@@ -11,26 +11,182 @@ const SignUp = ({ setMyUserId, setIsLoggedin, setDataUser, setAdmin }) => {
   const [password, setPassword] = useState("");
   const [username, setUsername] = useState("");
   const [valueError, setValueError] = useState("");
+  const [compteurNewPassword, setCompteurNewPassword] = useState(0);
+  const [classNameNewPassWord, setClassNameNewPassWord] = useState("");
+  const [compteurUppercase, setCompteurUppercase] = useState(0);
+  const [classNameUppercase, setClassNameUppercase] = useState("");
+  const [compteurLowercase, setCompteurLowercase] = useState(0);
+  const [classNameLowercase, setClassNameLowercase] = useState("");
+  const [compteurNumber, setCompteurNumber] = useState(0);
+  const [classNameNumber, setClassNameNumber] = useState("");
+  const [compteurCharacter, setCompteurCharacter] = useState(0);
+  const [classNameCharacter, setClassNameCharacter] = useState("");
+  const [compteurUserName, setCompteurUserName] = useState(0);
+  const [classNameUserName, setClassNameUserName] = useState("");
+  const [activeUppercase, setActiveUppercase] = useState(false);
+  const [active, setActive] = useState(false);
+  const clickOutSide = useRef();
+  const clickOutSideError = useRef();
   const history = useHistory();
+  const [verifGroupo, setVerifGroupo] = useState("");
+  const [classNameGroupo, setClassNameGroupo] = useState("");
+
+  const handleClickOutside = (e) => {
+    if (!clickOutSide.current?.contains(e.target)) {
+      setActiveUppercase(false);
+    }
+  };
+
+  const handleClickOutsideError = (e) => {
+    if (!clickOutSideError.current?.contains(e.target)) {
+      setActive(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  });
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutsideError);
+    return () => document.removeEventListener("mousedown", handleClickOutsideError);
+  });
+
+  useEffect(() => {
+    if (compteurNewPassword < 8) {
+      setClassNameNewPassWord("color-red");
+    } else {
+      setClassNameNewPassWord("color-green");
+    }
+    if (compteurUppercase < 1) {
+      setClassNameUppercase("color-red");
+    } else {
+      setClassNameUppercase("color-green");
+    }
+    if (compteurLowercase < 1) {
+      setClassNameLowercase("color-red");
+    } else {
+      setClassNameLowercase("color-green");
+    }
+    if (compteurNumber < 1) {
+      setClassNameNumber("color-red");
+    } else {
+      setClassNameNumber("color-green");
+    }
+    if (compteurCharacter < 1) {
+      setClassNameCharacter("color-red");
+    } else {
+      setClassNameCharacter("color-green");
+    }
+    if (compteurUserName < 4 || compteurUserName > 12) {
+      setClassNameUserName("color-red");
+    } else {
+      setClassNameUserName("color-green");
+    }
+    if (verifGroupo) {
+      let matchGroupo = verifGroupo.split("@");
+
+      if (matchGroupo[1] !== "groupomania.com") {
+        setClassNameGroupo("color-red");
+      } else {
+        setClassNameGroupo("color-green");
+      }
+    }
+  }, [
+    compteurNewPassword,
+    compteurUppercase,
+    compteurLowercase,
+    compteurNumber,
+    compteurCharacter,
+    compteurUserName,
+    verifGroupo,
+  ]);
 
   const onChangeEmail = (e) => {
+    setVerifGroupo(e.target.value);
+
     setEmail(e.target.value);
     setValueError("");
   };
 
   const onChangePassword = (e) => {
+    const regexUppercase = /[A-Z]/g;
+    let findLengthUppercase = e.target.value.match(regexUppercase);
+    if (findLengthUppercase) {
+      findLengthUppercase = findLengthUppercase.length;
+    }
+    const regexLowercase = /[a-z]/g;
+    let findLengthLowercase = e.target.value.match(regexLowercase);
+    if (findLengthLowercase) {
+      findLengthLowercase = findLengthLowercase.length;
+    }
+    const regexNumber = /[0-9]/g;
+    let findLengthNumber = e.target.value.match(regexNumber);
+    if (findLengthNumber) {
+      findLengthNumber = findLengthNumber.length;
+    }
+    const regexCharacter = /[_\W]/g;
+    let findLengthCharacter = e.target.value.match(regexCharacter);
+    if (findLengthCharacter) {
+      findLengthCharacter = findLengthCharacter.length;
+    }
+    setCompteurCharacter(findLengthCharacter);
+    setCompteurNumber(findLengthNumber);
+    setCompteurLowercase(findLengthLowercase);
+    setCompteurUppercase(findLengthUppercase);
+    setCompteurNewPassword(e.target.value.length);
     setPassword(e.target.value);
+    setActiveUppercase(true);
     setValueError("");
   };
 
   const onChangeUsername = (e) => {
+    setCompteurUserName(e.target.value.length);
     setUsername(e.target.value);
     setValueError("");
   };
 
   const onSignUp = async () => {
     let token;
+    if (compteurNewPassword < 8) {
+      setActiveUppercase(false);
+      setActive(true);
+      return;
+    }
+    if (compteurUppercase < 1) {
+      setActiveUppercase(false);
+      setActive(true);
+      return;
+    }
+    if (compteurLowercase < 1) {
+      setActiveUppercase(false);
+      setActive(true);
+      return;
+    }
+    if (compteurNumber < 1) {
+      setActiveUppercase(false);
+      setActive(true);
+      return;
+    }
+    if (compteurCharacter < 1) {
+      setActiveUppercase(false);
+      setActive(true);
+      return;
+    }
+    if (compteurUserName < 4 || compteurUserName > 12) {
+      setActiveUppercase(false);
+      setActive(true);
+      return;
+    }
+    if (verifGroupo) {
+      let matchGroupo = verifGroupo.split("@");
 
+      if (matchGroupo[1] !== "groupomania.com") {
+        setActive(true);
+        return;
+      }
+    }
     try {
       const response = await api.post("/users/register", {
         email,
@@ -59,6 +215,15 @@ const SignUp = ({ setMyUserId, setIsLoggedin, setDataUser, setAdmin }) => {
       } catch (error) {
         toastTrigger("error", "une erreur est survenu");
       }
+      setCompteurUserName(0);
+      setCompteurUppercase(0);
+      setCompteurNumber(0);
+      setCompteurLowercase(0);
+      setCompteurCharacter(0);
+      setCompteurNewPassword(0);
+      setVerifGroupo("");
+      setActiveUppercase(false);
+      setActive(false);
       history.push("/");
     } catch (error) {
       setValueError(error.response.data.error);
@@ -67,20 +232,40 @@ const SignUp = ({ setMyUserId, setIsLoggedin, setDataUser, setAdmin }) => {
 
   return (
     <div className="lmj-main">
-      <div className="lmj-flex">
+      <div ref={clickOutSide} className="lmj-flex">
         <div className="element-size">
           <Input onChange={onChangeEmail} value={email} label="Email" type="email" />
+          <div>{verifGroupo && <div className={classNameGroupo}>Doit finir par "@groupomania.com" </div>}</div>
         </div>
         <div className="element-marge  element-size">
           <Input onChange={onChangePassword} value={password} label="Password" type="password" />
+          <div>
+            {compteurNewPassword > 0 && (
+              <div className={classNameNewPassWord}>Minimum de caratère : {compteurNewPassword}/8</div>
+            )}
+          </div>
+          <div>{activeUppercase && <div className={classNameUppercase}>Minimum 1 majuscule </div>}</div>
+          <div>{activeUppercase && <div className={classNameLowercase}>Minimum 1 minuscule </div>}</div>
+          <div>{activeUppercase && <div className={classNameNumber}>Minimum 1 chiffre </div>}</div>
+          <div>{activeUppercase && <div className={classNameCharacter}>Minimum 1 charatère spécial </div>}</div>
         </div>
         <div className="element-marge  element-size">
           <Input onChange={onChangeUsername} value={username} label="Username" />
+          <div>
+            {compteurUserName > 0 && (
+              <div className={classNameUserName}>Minimum de caratère : {compteurUserName}/4</div>
+            )}
+          </div>
         </div>
         {valueError && <div>{valueError}</div>}
         <div className="element-marge">
           <Button onClick={onSignUp} title="Valider" />
         </div>
+        {active && (
+          <div ref={clickOutSideError} className="color-red">
+            Vériffiez vos information de saisie
+          </div>
+        )}
       </div>
     </div>
   );
